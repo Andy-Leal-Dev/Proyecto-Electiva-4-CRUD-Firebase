d5da82fdb0a88a3a2ab1f1331394017e11dd3e95
Andrierber Leal <lealandrierber343@gmail.com>
2024-06-06 06:43:18 -0400

    V-0.2

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
- import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
 
- // Your web app's Firebase configuration
- // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+ import { getFirestore, collection, addDoc, getDocs, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+
  const firebaseConfig = {
    apiKey: "AIzaSyC1AahhmF5o4VBqb5iqBPPDTUYZG-P_WXc",
    authDomain: "proyectouni-7faff.firebaseapp.com",
@@ -16,10 +14,39 @@
    measurementId: "G-MWTR4GEZWE"
  };
 
- // Initialize Firebase
+
  const app = initializeApp(firebaseConfig);
- const analytics = getAnalytics(app);
+  const db = getFirestore(app);
 
+      
+        const addBook = async (title, author) => {
+            const booksRef = collection(db, "books");
+            const newBook = {
+                title,
+                author
+            };
+            await addDoc(booksRef, newBook);
+            console.log("Book added successfully!");
+        }
 
+        const getBooks = async () => {
+            const booksRef = collection(db, "books");
+            const querySnapshot = await getDocs(booksRef);
+            querySnapshot.forEach((doc) => {
+                console.log(doc.id, " => ", doc.data());
+            });
+        }
 
- 
+        const updateBook = async (bookId, newTitle) => {
+            const bookRef = doc(db, "books", bookId);
+            await updateDoc(bookRef, {
+                title: newTitle
+            });
+            console.log("Book updated successfully!");
+        }
+
+        const deleteBook = async (bookId) => {
+            const bookRef = doc(db, "books", bookId);
+            await deleteDoc(bookRef);
+            console.log("Book deleted successfully!");
+        }
